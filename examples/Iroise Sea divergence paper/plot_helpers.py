01d9db008cc5cec909aae2bc79526bba5fc18ee3
Jakub Michalski <jmichalski3009@gmail.com>
2026-01-23 12:22:30 +0100

    remove unused commented out code, linting

--- examples/Iroise Sea divergence paper/plot_helpers.py
+++ examples/Iroise Sea divergence paper/plot_helpers.py
@@ -97,7 +97,6 @@ def add_track_shape(OSCAR, ax, sel_for_cutting, color="red", alpha=0.3, linewidt
     )
 
 
-
 def extract_transect_range(current_transect):
     """Extracts the range of the current transect"""
     coordinates = {}
@@ -679,17 +678,7 @@ def plot_MARS2D_and_MARS3D_profiles(
         xlabel="Horizontal current velocity [m/s]",
         ylabel="Depth as fraction of total depth $h$",
     )
-    # plot_vertical(
-    #     bottom_axes[2],
-    #     MARS3D,
-    #     "CurrentDirection",
-    #     title="MARS3D horizontal current direction vs depth",
-    #     points=points,
-    #     xlabel="Direction [$°$]",
-    #     ylabel="Depth as fraction of total depth $h$",
-    # )
     bottom_axes[1].set_xlim(0, 3)
-    # bottom_axes[2].set_xlim(0, 360)
 
     plt.subplots_adjust(
         left=0.1, right=0.9, top=0.92, bottom=0.1, hspace=0.2, wspace=0.25
