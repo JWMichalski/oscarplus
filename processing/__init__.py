764d8a8e804c2215c9847e8ec97dbe8f684262c3
Jakub Michalski <jmichalski3009@gmail.com>
2026-09-15 11:58:26 +0100

    add new processing module: tidal_analysis
    With new function split_dataset_by_phase

--- processing/__init__.py
+++ processing/__init__.py
@@ -2,3 +2,4 @@ from oscarplus.processing import filtering
 from oscarplus.processing import secondary_products
 from oscarplus.processing import level1
 from oscarplus.processing import level2
+from oscarplus.processing import tidal_analysis
